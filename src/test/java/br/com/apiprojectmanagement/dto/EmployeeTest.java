7da30aedce4d5f10f3cc6334f08a6f30f682c51e
Ramon Barros <ramondesbarros@gmail.com>
2022-04-14 22:17:07 -0300

    add test class

---
+++ src/test/java/br/com/apiprojectmanagement/dto/EmployeeTest.java
@@ -0,0 +1,5 @@
+package br.com.apiprojectmanagement.dto;
+
+public class EmployeeTest {
+
+}
---
+++ src/test/java/br/com/apiprojectmanagement/service/impl/EmployeeServiceImplTest.java
@@ -0,0 +1,12 @@
+package br.com.apiprojectmanagement.service.impl;
+
+import org.junit.Test;
+
+public class EmployeeServiceImplTest {
+
+	
+	@Test
+	public void createEmployeeTest() {
+
+	}
+}
